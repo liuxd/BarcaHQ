526c018fbe2f2595dd6fea1f0e25e33861b78b60
Allen Liu <Allen@vesta-central.com>
2017-10-04 16:43:45 +1300

    Refactor the grid part.

--- app/render.js
+++ app/render.js
@@ -1,26 +1,3 @@
-let tbodyHandler = function(target_id, rows, rowClassHandler) {
-  let body = ""
-
-  for (let row of rows) {
-    let rowClass = rowClassHandler(row)
-    let tr = "<tr>"
-
-    if (rowClass) {
-      tr = "<tr class='" + rowClass + "'>"
-    }
-
-    for (let td of row) {
-      tr += "<td>" + td + "</td>"
-    }
-
-    tr += "</td>"
-
-    body += tr
-  }
-
-  $("#" + target_id).html(body)
-}
-
 new Vue({
   el: "#app",
   data: {
@@ -40,7 +17,63 @@ new Vue({
     ranking_grid: true,
     players_grid: false,
     rankingActive: true,
-    playersActive: false
+    playersActive: false,
+    ranking_list: [],
+    player_list: []
+  },
+  created: function() {
+    let that = this
+
+    // Ranking List
+    $.get("http://www.espnfc.com/spanish-primera-division/15/table", function(response) {
+      let html = $(response)
+      let teamList = []
+
+      html.find("#tables-overall table tbody tr").each(function() {
+        let row = $(this).children()
+        let team = []
+
+        row.each(function() {
+          team.push($(this).text())
+        })
+
+        if (team[0] !== "POS") {
+          teamList.push([
+            team[0],
+            team[1].replace(/(^\s*)|(\s*$)/g, ""),
+            team[3],
+            team[4],
+            team[5],
+            team[6],
+            team[7],
+            team[8],
+            team[22],
+            team[23]
+          ])
+        }
+      })
+
+      that.ranking_list = teamList
+    })
+
+    // Player List
+    $.get("http://www.espnfc.com/spanish-primera-division/15/statistics/scorers", function(response) {
+      let html = $(response)
+      let playerList = []
+
+      html.find("#stats-top-scorers table tbody tr").each(function() {
+        let row = $(this).children()
+        let player = []
+
+        row.each(function() {
+          player.push($(this).text())
+        })
+
+        playerList.push(player)
+      })
+
+      that.player_list = playerList
+    })
   },
   computed: {
     banner: function() {
@@ -85,47 +118,7 @@ new Vue({
         this.rankingActive = true
       }
     },
-    ranking_list: function() {
-      let tbody = ""
-
-      $.get("http://www.espnfc.com/spanish-primera-division/15/table", function(response) {
-        let html = $(response)
-        let teamList = []
-
-        html.find("#tables-overall table tbody tr").each(function() {
-          let row = $(this).children()
-          let team = []
-
-          row.each(function() {
-            team.push($(this).text())
-          })
-
-          if (team[0] !== "POS") {
-            teamList.push([
-              team[0],
-              team[1].replace(/(^\s*)|(\s*$)/g, ""),
-              team[3],
-              team[4],
-              team[5],
-              team[6],
-              team[7],
-              team[8],
-              team[22],
-              team[23]
-            ])
-          }
-        })
-
-        tbody = tbodyHandler("ranking", teamList, function(row) {
-          return row[1] === "Barcelona" ? "success" : ""
-        })
-      })
-
-      return tbody
-    },
     player_list: function() {
-      let tbody = ""
-
       $.get("http://www.espnfc.com/spanish-primera-division/15/statistics/scorers", function(response) {
         let html = $(response)
         let playerList = []
@@ -140,13 +133,7 @@ new Vue({
 
           playerList.push(player)
         })
-
-        tbody = tbodyHandler("players", playerList, function(row) {
-          return row[2] === "Barcelona" ? "success" : ""
-        })
       })
-
-      return tbody
     }
   }
 })
